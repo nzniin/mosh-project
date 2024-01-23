ba84d02c30f37f058c8ecf478fdb9dda9936c78c
nazanin <dev.nzniing@gmail.com>
2024-01-23 18:38:00 +0330

    Add a drop-down menu

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,8 +8,8 @@ const handler = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
-            clientId: "...",
-            clientSecret: "...",
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
     session: {
