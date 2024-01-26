7fa1adcf8db8b8a8b441bd8f92a398c9f2275266
nazanin <dev.nzniing@gmail.com>
2024-01-26 16:32:43 +0330

    Optimize performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -8,18 +8,22 @@ import { getServerSession } from 'next-auth';
 import authOptions from '@/app/auth/authOptions';
 import AssigneeSelect from './AssigneeSelect';
 import { describe } from 'node:test';
+import { cache } from 'react';
 
 
 interface Props {
     params: { id: string }
 }
 
+const fetchUser = cache(async (issueId: number) => 
+  prisma.issue.findUnique({
+        where: {id: issueId}
+    }))
+
 async function IssueDetailPage({params}: Props) {
     const session = await getServerSession(authOptions)
 
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id)}
-    });
+    const issue = await fetchUser(parseInt(params.id));
 
     if (!issue)
         notFound();
@@ -41,9 +45,8 @@ async function IssueDetailPage({params}: Props) {
 }
 
 export async function generateMetadata({ params }: Props){
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id)}
-    })
+    const issue = await fetchUser(parseInt(params.id));
+    
 
     return {
         title: issue?.title,
